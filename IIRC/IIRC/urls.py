3f4f64570741c69cb1758822de676f858620b7a4
shah <shah@django.com>
2018-12-26 16:22:10 -0800

    Added models for blog, user acount and comments and alanding page and a contact page

---
+++ IIRC/IIRC/urls.py
@@ -0,0 +1,42 @@
+"""IIRC URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf import settings
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from blogs import views
+
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r"^blog/", include("pinax.blog.urls", namespace="pinax_blog")),
+    url(r"^ajax/images/", include("pinax.images.urls", namespace="pinax_images")),
+    url(r"^account/", include("account.urls")),
+    url(r"^comments/", include("pinax.comments.urls", namespace="pinax_comments"))
+]
+
+urlpatterns += [
+    url(r'^blogs/', include('blogs.urls')),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^base/', views.index, name='index'),
+    url(r'^', views.land, name='land'),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ IIRC/blogs/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from .form import SignUpForm
+from .form import ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Post
+
+def home(request):
+    # return render(request, 'base.html', {})
+    return render(request, 'homenew.html', {})
+
+
+def land(request):
+    return render(request, 'landing.html', {})
+
+
+def index(request):
+    title = "Welcome guest"
+    form = SignUpForm(request.POST or None)
+    if request.user.is_authenticated:
+        title = "Welcome %s" % (request.user)
+    context = {
+        'title': title,
+        'myName': 'Hasan',
+        'form': form,
+    }
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if not instance.fullname:
+            email_base, provider = instance.email.split('@')
+            instance.fullname = email_base
+            instance.save()
+        context = {
+            'title': 'Thank for Registeration',
+        }
+    return render(request, 'base.html', context)
+    # return render(request, 'index.html', context)
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form_email = form.cleaned_data.get('email')
+        form_message = form.cleaned_data.get('message')
+        form_fullname = form.cleaned_data.get('fullname')
+
+        subject = 'Mail from Django'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, 'hasanshah5882@gmail.com']
+        contact_message = '%s: %s via %s'%(
+            form_fullname,
+            form_message,
+            form_email)
+        send_mail(subject,
+                  contact_message,
+                  from_email,
+                  to_email,
+                  fail_silently=False)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'forms.html', context)
+
+
+class Postclass():
+    class Meta:
+        model = Post
+        fields = ['email', 'fullname']
+
+
+def post(request):
+
+    return render(request, 'home.html', {})
+
